bbd7e62df95c2d647e6789882efd2f8719d63f73
Максим Ситников <maksimsitnikov@MacBook-Air-Maksim.local>
2021-09-22 21:22:40 +0300

    Практическая работа 13

--- models/user.js
+++ models/user.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema({
       validator(link) {
         return /^(http|https):\/\/[^ "]+$/g.test(link);
       },
-      message: 'Некорректный URL',
+      message: "Некорректный URL",
     },
   },
 });
