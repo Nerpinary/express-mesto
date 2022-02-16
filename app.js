749c1e176932314a01b26620ca3297477382468d
Максим Ситников <maksimsitnikov@Air-Maksim.Dlink>
2022-02-16 22:03:02 +0300

    add CORS

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors')
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const { celebrate, Joi, errors } = require('celebrate');
@@ -9,13 +10,18 @@ const { login } = require('./controllers/users');
 const { createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const NotFoundError = require('./errors/not_found_error');
-const allowedCors = [
-  'https://praktikum.tk',
-  'http://praktikum.tk',
-  'localhost:3000',
-  'https://nerpinary.nomoredomains.club',
-  'http://nerpinary.nomoredomains.club'
-];
+const options = {
+  origin: [
+    'http://localhost:3000',
+    'https://nerpinary.nomoredomains.club',
+    'http://nerpinary.nomoredomains.club'
+  ],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+  credentials: true,
+};
 
 const app = express();
 const { PORT = 3000 } = process.env;
@@ -24,42 +30,10 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
+app.use('*', cors(options));
 app.use(express.json());
 app.use(helmet());
 app.use(cookieParser());
-
-app.use((request, response, next) => {
-  request.user = {
-    _id: '620d47d95da04d2b46620f54',
-  };
-
-  next();
-});
-
-app.use(function (req,res,next) {
-  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
-
-// Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
-  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
-
-// Если это предварительный запрос, добавляем нужные заголовки
-  if (method === 'OPTIONS') {
-    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-  }
-  next();
-})
-
-app.use(function(req, res, next) {
-  const { origin } = req.headers;
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  } else {
-    res.header('Access-Control-Allow-Origin', '*');
-  }
-  next();
-});
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
